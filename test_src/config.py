0793db53e1fd8af087b5eb61b2532fc32a92ab29
pyohei <mukaishohei76@gmail.com>
2018-06-16 21:58:25 +0900

    Delete unused test script.

--- test_src/config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python
-# -*- coding: utf-8 -*-
-
-""" This is test source """
-
-HOGEHOGE = "config"
-
-class HOGE:
-    pass
--- test_src/src/lib/db/db.py
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/python
-# -*- coding: utf-8 -*-
-
-import config
-from src.src import HOGEHOGE
-from src.lib.lib import HOGEHOGE
-from src.lib import lib
